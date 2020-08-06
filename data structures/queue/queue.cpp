a4367af22d2fc4c0f6435f4747683dc753dba03f
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-08-06 10:04:27 +0530

    "priority_queue_data structure"

--- data structures/queue/queue.cpp
+++ data structures/queue/queue.cpp
@@ -4,20 +4,19 @@ using namespace std;
 int main(){
 	int n,x,i;
 	cin>>n;
-	queue<int> q;
-	queue<int> p;
+	priority_queue<int> q;
+	//priority_queue<int> p;
 	
 	for(i=0;i<n;i++){
 		cin>>x;
 		q.push(x);	//push-add end of the element
 	}
-	cout<<q.front()<<" "<<q.back()<<endl; //back - last element of queue
-	cout<<q.size()<<endl;	//size- size of queue
+	cout<<q.size()<<endl;	//size- size of priority_queue
 	while(!q.empty()){		//empty
-		x = q.front();		//front- front element of queue
-		q.pop();			//pop
+		x = q.top();		//top - first element priority_queue
+		q.pop();			//pop - remove element
 		cout<<x<<" ";
 	}
-	p.swap(q);				//swap two queue
+//	p.swap(q);				//swap two queue
 	return 0;
 }
