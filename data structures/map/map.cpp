cab979530a84b4a7f436b6a8759527db6db38f5f
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-08-06 17:50:40 +0530

    "map"

---
+++ data structures/map/map.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<map>
+using namespace std;
+int main(){
+	int n,x,i;
+	cin>>n;
+	map<int,int> m;  		//set declaration
+	// m.insert({key,value})
+	// m.insert(iterator pos,({key,value}))
+	// m1.insert(m.begin(), m.end());
+	
+	// m.find(value)
+	// m.at(key)
+	
+	// m1.swap(m2)
+	// m.size();
+	// m.max_size()
+	
+	// m.empty()
+	// m.erase(key)
+	// m.erase(iterator)
+	// m.clear()
+	
+	// m.count(key)
+	// m.lower_bound(2);
+	// mp.upper_bound(2);
+	return 0;
+}
