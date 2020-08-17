3968a0a4758fd6f9730b22391e46c7515bd4d859
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-08-17 12:00:02 +0530

    ""second largest"

---
+++ codechef/practice beginner/second largest.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	long long int t,a,b,c;
+	cin>>t;
+	while(t--){
+		cin>>a>>b>>c;
+		if(a>=b && a>=c){
+			if(b>=c)
+				cout<<b<<endl;
+			else
+				cout<<c<<endl;
+		}else if(b>=a && b>=c){
+			if(a>=c)
+				cout<<a<<endl;
+			else
+				cout<<c<<endl;
+		}else if(a>=b){
+			cout<<a<<endl;			
+		}else
+			cout<<b<<endl;
+	}
+	return 0;
+}
