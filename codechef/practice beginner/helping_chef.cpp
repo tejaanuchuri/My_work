943ce46ee34bfcab910db447815bb8eae3b3cf5b
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-08-17 13:35:10 +0530

    "helping chef"

---
+++ codechef/practice beginner/helping_chef.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+	long long int t,n;
+	cin>>t;
+	while(t--){
+		cin>>n;
+		if(n<10)
+		cout<<"Thanks for helping Chef!"<<endl;
+		else
+			cout<<"-1"<<endl;
+					
+	}
+	return 0;
+}
