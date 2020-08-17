32f885932a72dc279568d6c07de9d61328b957be
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-08-17 09:50:19 +0530

    "» Add Two Numbers"

---
+++ codechef/practice beginner/Add Two Numbers.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<malloc.h>
+using namespace std;
+int main(){
+	long long int t,a,b;
+		cin>>t;
+		while(t--){
+			cin>>a>>b;
+			cout<<(a+b)<<endl;	
+		}	
+	return 0;
+}
