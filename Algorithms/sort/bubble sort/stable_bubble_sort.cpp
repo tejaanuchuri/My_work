0b6f62580a32eb6b930cb6859ef0778cb24e4f31
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-08-20 23:02:13 +0530

    "stable bubble sort"

---
+++ Algorithms/sort/bubble sort/stable_bubble_sort.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print(long long int arr[100],long long int n){
+	for(long long int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+	return ;
+}
+void selection_sort(long long int arr[100],long long int n){
+	print(arr,n);
+	for(int i=0;i<n-1;i++){
+		int flag = 1;
+		for(int j=0;j<n-1-i;j++){
+			if(arr[j]>arr[j+1]){
+				swap(arr[j],arr[j+1]);
+				flag=0;
+			}
+		}
+		if(flag==1)
+			break;
+	}
+	print(arr,n);
+	return ;
+}
+int main(){
+	long long int t,n,i;
+	cin>>n;
+	long long int arr[n];
+	for(i=0;i<n;i++)
+		cin>>arr[i];
+	selection_sort(arr,n);
+	return 0;
+}
