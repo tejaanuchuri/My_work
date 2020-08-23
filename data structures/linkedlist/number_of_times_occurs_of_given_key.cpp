90942e2810057e17f4cb5996b5d186a14617a753
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-08-23 21:01:03 +0530

    "number of times occurs of a given key"

---
+++ data structures/linkedlist/number_of_times_occurs_of_given_key.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<malloc.h>
+using namespace std;
+struct node{
+	int data;
+	struct node *next;
+};
+struct node* new_node(int data){
+	struct node* newnode = (struct node*)malloc(sizeof(struct node));
+	newnode->data = data;
+	newnode->next = NULL;
+	return newnode;
+}
+struct node* insert_end(struct node* head,int data){
+	struct node* newnode = new_node(data);
+	if(head==NULL)
+		return newnode;
+	else{
+		struct node* temp = head;
+		while(temp->next!=NULL){
+			temp = temp->next;
+		}
+		temp->next = newnode;
+	}
+	return head;
+}
+int count_of_occurs(struct node* head,int key){
+	int count=0;
+	while(head!=NULL){
+		if(head->data==key)
+			count++;
+		head=head->next;
+	}
+	return count;
+}
+int main(){
+	struct node* head=NULL;
+	int n,i,x;	
+	cin>>n;
+	for(i=0;i<n;i++){
+		cin>>x;
+		head = insert_end(head,x);
+	}
+	cin>>x;
+	cout<<"count of given element =>>> "<<count_of_occurs(head,x);
+	return 0;
+}
