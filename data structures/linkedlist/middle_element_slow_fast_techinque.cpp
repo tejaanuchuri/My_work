3798b5ab625ce51c810516c3d579952befd8f3b2
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-08-23 20:14:41 +0530

    "middle element slow fast techinque "

---
+++ data structures/linkedlist/middle_element_slow_fast_techinque.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<malloc.h>
+using namespace std;
+struct node{
+	int data;
+	struct node *next;
+};
+struct node* new_node(int data){
+	struct node* newnode = (struct node*)malloc(sizeof(struct node));
+	newnode->data = data;
+	newnode->next = NULL;
+	return newnode;
+}
+struct node* insert_end(struct node* head,int data){
+	struct node* newnode = new_node(data);
+	if(head==NULL)
+		return newnode;
+	else{
+		struct node* temp = head;
+		while(temp->next!=NULL){
+			temp = temp->next;
+		}
+		temp->next = newnode;
+	}
+	return head;
+}
+
+void middle_element(struct node* head){
+	if(head==NULL)	
+		return ;
+	struct node* slow = head;
+	struct node* fast = head;
+	while(fast!=NULL && fast->next!=NULL){
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	cout<<"middle element =>>> "<<slow->data<<endl;
+	return ;
+}
+int main(){
+	struct node* head=NULL;
+	int n,i,x;	
+	cin>>n;
+	for(i=0;i<n;i++){
+		cin>>x;
+		head = insert_end(head,x);
+	}
+	middle_element(head);
+	return 0;
+}
