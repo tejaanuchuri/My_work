f1f5801ba92a16013c8aeedb9428130dea69ca27
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-08-23 19:11:36 +0530

    "search a key in linkedlist"

---
+++ data structures/linkedlist/search_key_in_linkedlist.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<malloc.h>
+using namespace std;
+struct node{
+	int data;
+	struct node *next;
+};
+struct node* new_node(int data){
+	struct node* newnode = (struct node*)malloc(sizeof(struct node));
+	newnode->data = data;
+	newnode->next = NULL;
+	return newnode;
+}
+struct node* insert_front(struct node* head,int data){
+	struct node* newnode = new_node(data);
+	newnode->next = head;
+	return newnode;
+}
+
+bool search(struct node* head,int key){
+	while(head!=NULL){
+		if(head->data==key)
+			return 1;
+		head= head->next;
+	}
+	return 0;
+}
+int main(){
+	struct node* head=NULL;
+	int n,i,x;	
+	cin>>n;
+	for(i=0;i<n;i++){
+		cin>>x;
+		head = insert_front(head,x);
+	}
+	
+	cin>>x;
+	if(search(head,x))
+		cout<<"found"<<endl;
+	else
+		cout<<"not found"<<endl;
+	return 0;
+}
