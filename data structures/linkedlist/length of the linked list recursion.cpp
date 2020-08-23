fb10d2ba554de328fcbc9d925106cb832c4e9239
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-08-23 19:05:43 +0530

    "length of the linkedlist recursion"

---
+++ data structures/linkedlist/length of the linked list recursion.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<malloc.h>
+using namespace std;
+struct node{
+	int data;
+	struct node *next;
+};
+int length_of_linkedlist(struct node* head){
+	if(head==NULL)
+		return 0;
+	return 1 + length_of_linkedlist(head->next);
+}
+struct node* new_node(int data){
+	struct node* newnode = (struct node*)malloc(sizeof(struct node));
+	newnode->data = data;
+	newnode->next = NULL;
+	return newnode;
+}
+struct node* insert_front(struct node* head,int data){
+	struct node* newnode = new_node(data);
+	newnode->next = head;
+	return newnode;
+}
+int main(){
+	struct node* head=NULL;
+	int n,i,x;	
+	cin>>n;
+	for(i=0;i<n;i++){
+		cin>>x;
+		head = insert_front(head,x);
+	}
+	cout<<"length of the linkedlist==>  "<<length_of_linkedlist(head)<<endl;
+	return 0;
+}
