9050d8a977f4371488c981cba465897943816b67
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-08-24 12:13:22 +0530

    "reverse of a linkedlist with o(1) space"

---
+++ data structures/linkedlist/reverse_of_a_linkedlist/o(n)_o(1).cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<malloc.h>
+using namespace std;
+struct node{
+	int data;
+	struct node *next;
+};
+struct node* new_node(int data){
+	struct node* newnode = (struct node*)malloc(sizeof(struct node));
+	newnode->data = data;
+	newnode->next = NULL;
+	return newnode;
+}
+struct node* insert_end(struct node* head,int data){
+	struct node* newnode = new_node(data);
+	if(head==NULL)
+		return newnode;
+	else{
+		struct node* temp = head;
+		while(temp->next!=NULL){
+			temp = temp->next;
+		}
+		temp->next = newnode;
+	}
+	return head;
+}
+void print_list(struct node* head){
+	while(head!=NULL){
+		if(head->next==NULL)	
+			cout<<head->data<<endl;
+		else{
+			cout<<head->data<<"->";	
+		}
+		head=head->next;
+	}
+	return ;
+}
+struct node* reverse(struct node* head){
+	if(head==NULL || head->next==NULL)
+		return head;
+	struct node* cur = head;
+	struct node* prev = NULL;
+	struct node* next = NULL;
+	while(cur!=NULL){
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;
+	}
+	head = prev;
+	return head;
+}
+int main(){
+	struct node* head=NULL;
+	int n,i,x;	
+	cin>>n;
+	for(i=0;i<n;i++){
+		cin>>x;
+		head = insert_end(head,x);
+	}
+	print_list(head);
+	head = reverse(head);
+	print_list(head);
+	return 0;
+}
