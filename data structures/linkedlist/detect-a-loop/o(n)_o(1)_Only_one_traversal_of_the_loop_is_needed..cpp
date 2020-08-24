6615e148e19f0b112c71c2420df2a343f9c03f20
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-08-24 09:12:37 +0530

    "o(n)o(1)Only_one_traversal_of_the_loop_is needed"

---
+++ data structures/linkedlist/detect-a-loop/o(n)_o(1)_Only_one_traversal_of_the_loop_is_needed..cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<malloc.h>
+using namespace std;
+struct node{
+	int data;
+	struct node *next;
+};
+struct node* new_node(int data){
+	struct node* newnode = (struct node*)malloc(sizeof(struct node));
+	newnode->data = data;
+	newnode->next = NULL;
+	return newnode;
+}
+struct node* insert_end(struct node* head,int data){
+	struct node* newnode = new_node(data);
+	if(head==NULL)
+		return newnode;
+	else{
+		struct node* temp = head;
+		while(temp->next!=NULL){
+			temp = temp->next;
+		}
+		temp->next = newnode;
+	}
+	return head;
+}
+bool detect_loop(struct node* head){
+	struct node* temp = new_node(-1);
+	while(head!=NULL){
+		if(head->next==NULL)
+			return false;
+		if(head->next==temp)
+			return true;
+		struct node* next = head->next;
+		head->next = temp;
+		head = next;
+	}
+	return false;
+}
+int main(){
+	struct node* head=NULL;
+	int n,i,x;	
+	cin>>n;
+	for(i=0;i<n;i++){
+		cin>>x;
+		head = insert_end(head,x);
+	}
+	head->next->next->next = head->next;
+	if(detect_loop(head))	
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
+	return 0;
+}
